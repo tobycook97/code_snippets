952773592e83cf5ddb3f4d11a34ee7ffdd8e08f5
Toby Cook <toby.cook@test-and-trace.nhs.uk>
2021-07-21 18:58:01 +0100

    Tutorial 7 Video complete

--- Django_Blog/django_project/users/views.py
+++ Django_Blog/django_project/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST': #Post is a type of Http request. (GET is anoter one)
@@ -8,11 +9,15 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username') # cleaned datat is styuff which has been converted to pyhton types from the form input :) 
-            messages.success(request, f'Account created for {username}!') #we add one of these messages and it prints in blog base.html
+            messages.success(request, f'your account has been created, you are now able to login!!') #we add one of these messages and it prints in blog base.html
             return redirect('blog-home')
     else:
         form = UserRegisterForm()
     return render(request,"users/register.html", {'form':form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
 
 
 #different types of messages:
